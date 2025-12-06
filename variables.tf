966a9fde4adb037880d8f4b89342c259f3a806da
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-06 08:42:51 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -2,4 +2,5 @@ module "vpc" {
     source = "../terraform-aws-vpc-p1"
     project_name = var.project_name
     environment = var.environment
+    common_tags = var.common_tags
 }
