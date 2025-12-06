a79ba77ae27702b94300df4fae633ae8485e4395
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-06 20:09:56 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -8,20 +8,20 @@ variable "environment" {
 
 variable "common_tags" {
   default = {
-    Project = "expense"
-    Terraform = "true"
+    Project     = "expense"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
 variable "public_subnet_cidrs" {
-  default = ["10.0.1.0/24","10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  default = ["10.0.11.0/24","10.0.12.0/24"]
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
-  default = ["10.0.21.0/24","10.0.22.0/24"]
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-    source = "../terraform-aws-vpc-p1"
-    project_name = var.project_name
-    environment = var.environment
-    common_tags = var.common_tags
-    public_subnet_cidrs = var.public_subnet_cidrs
-    private_subnet_cidrs = var.private_subnet_cidrs
-    database_subnet_cidrs = var.database_subnet_cidrs
+  source                = "../terraform-aws-vpc-p1"
+  project_name          = var.project_name
+  environment           = var.environment
+  common_tags           = var.common_tags
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
 }
